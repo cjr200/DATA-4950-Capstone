72823fa981d402ef5adf5d225db95e47951bab79
cjr200 <calebjreed661@gmail.com>
2023-02-20 15:41:48 -0600

    cookiecutter setup

---
+++ capstone/tests/test_capstone.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Tests for `capstone` package."""
+
+
+import unittest
+from click.testing import CliRunner
+
+from capstone import capstone
+from capstone import cli
+
+
+class TestCapstone(unittest.TestCase):
+    """Tests for `capstone` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        assert 'capstone.cli.main' in result.output
+        help_result = runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--help  Show this message and exit.' in help_result.output
