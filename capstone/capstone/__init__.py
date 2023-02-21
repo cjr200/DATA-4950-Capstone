5d42d3f4f4365610adfde06cf6c4bcfc9e670c62
cjr200 <calebjreed661@gmail.com>
2023-02-20 20:36:50 -0600

    Revert "cookiecutter setup"
    This reverts commit 72823fa981d402ef5adf5d225db95e47951bab79.

--- capstone/capstone/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Capstone."""
-
-__author__ = """Caleb Reed"""
-__email__ = 'calebjreed661@gmail.com'
-__version__ = '0.1.0'
--- capstone/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Capstone in a project::
-
-    import capstone
